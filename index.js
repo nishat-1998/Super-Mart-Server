5bfbd900f0b50b03cf42e2652f05dc8c6249ac97
Nishat Ahmed <nishatahmed4485@gmail.com>
2021-04-01 17:29:56 +0100

    all

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const eventCollection = client.db("vol-net").collection("events");
+  const ordersCollection = client.db("vol-net").collection("orders");
   
   app.get('/events',(req,res) =>{
       eventCollection.find()
@@ -38,6 +39,22 @@ client.connect(err => {
       // console.log(" inserted Count",result.insertedCount)
        res.send(result.insertedCount >0)
    })
+    })
+
+    app.post('/addOrder',(req,res)=>{
+      const Order=req.body;
+      ordersCollection.insertOne(Order)
+      .then(result=>{
+        res.send(result.insertedCount >0);
+      })
+    })
+
+    app.get('/orders',(req,res) =>{
+      //console.log(req.query.email);
+      ordersCollection.find({email:req.query.email})
+      .toArray((err,documents) =>{
+        res.send(documents)
+      })
     })
     app.delete('/deleteEvent/:id',(req,res)=>{
       const id =ObjectId(req.params.id);
